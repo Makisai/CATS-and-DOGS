6d8f61edcab4dd51d46ac1541eabc525f663afc1
Makisai <jschmaglowski@aol.com>
2018-05-22 11:22:27 +0200

    new cat images, swipe functionality

---
+++ public/js/animations.js
@@ -0,0 +1,9 @@
+var dogsSlide = () => {
+  let containerD = document.getElementById("dogs");
+  containerD.className = "dogs dogs-animation";
+}
+
+var catsSlide = () => {
+  let containerD = document.getElementById("cats");
+  containerD.className = "cats cats-animation";
+}
